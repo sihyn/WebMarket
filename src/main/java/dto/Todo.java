b94490d1c470f254d34d6bf6ce45f80f9fbc7ccc
sihyn <sihyn126@gmail.com>
2022-05-23 17:05:56 +0900

    mvc2 pattern

--- src/main/java/dto/Todo.java
+++ src/main/java/dto/Todo.java
@@ -1,43 +1,60 @@
 package dto;
 
+import java.util.Objects;
+
 public class Todo {
-	private long id;
-	private String task;
-	private boolean isDone;
-	
-	public Todo(String task) {
-		this.task = task;
-	}
+    private long id;
+    private String task;
+    private boolean isDone;
 
-	public long getId() {
-		return id;
-	}
+    public Todo(String task) {
+        this.task = task;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public String getTask() {
-		return task;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public void setTask(String task) {
-		this.task = task;
-	}
+    public String getTask() {
+        return task;
+    }
 
-	public boolean isDone() {
-		return isDone;
-	}
+    public void setTask(String task) {
+        this.task = task;
+    }
 
-	public void setDone(boolean isDone) {
-		this.isDone = isDone;
-	}
+    public boolean isDone() {
+        return isDone;
+    }
 
-	@Override
-	public String toString() {
-		return "Todo [id=" + id + ", task=" + task + ", isDone=" + isDone + "]";
-	}
-	
-	
-	
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Todo other = (Todo) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id=" + id + ", task=" + task + ", isDone=" + isDone + "]";
+    }
+
 }
